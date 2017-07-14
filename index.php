8189c795e80d32ea92f902a600bd77790aa3fdbe
jlin53 <jlin53@ucsc.edu>
2017-07-14 03:08:00 +0000

    remove php script bug

--- index.php
+++ index.php
@@ -1,10 +1,4 @@
 
 <?php 
-if (!isset($_COOKIE['c4a_session_id'])) {
-    $new_id = session_create_id();
-    setCookie("c4a_session_id", $new_id, 8251005, '/');
-    exit();
-}
-
 header( 'Location: /app/index.html' ); 
 ?>
