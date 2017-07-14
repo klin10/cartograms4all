4615483b55ecfcdd336ebed64000858d41eaa4e1
jlin53 <jlin53@ucsc.edu>
2017-07-14 21:49:17 +0000

    DEPLOY

--- app/php/upload-manager.php
+++ app/php/upload-manager.php
@@ -3,18 +3,7 @@
 header('Access-Control-Allow-Origin: *');
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
-    // Check if file was uploaded without errors
-    if(isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0){
-        $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
-        $filename = $_FILES["photo"]["name"];
-        $filetype = $_FILES["photo"]["type"];
-        $filesize = $_FILES["photo"]["size"];
-    
-       // Verify file size - 5MB maximum
-        $maxsize = 5 * 1024 * 1024;
-        if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
-    
-        // Verify MYME type of the file
+            // Verify MYME type of the file
         if(in_array($filetype, $allowed)){
             // Check whether file exists before uploading it
             if(file_exists("upload/" . $_FILES["photo"]["name"])){
