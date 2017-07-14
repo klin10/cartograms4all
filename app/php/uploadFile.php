99a4f0b6b1a09aca0bc6d374ff72a8ba7d773ba5
jlin53 <jlin53@ucsc.edu>
2017-07-14 03:22:48 +0000

    Upload debug msg

--- app/php/uploadFile.php
+++ app/php/uploadFile.php
@@ -3,8 +3,8 @@
 
 //$sessionId = $_COOKIE['c4a_session_id'];
 $sessionId = 10;
-$targetDir = "/app/data";
-$targetFile = $target_dir . $sessionId . "_" . basename($_FILES["fileToUpload"]["name"]);
+$targetDir = "/data";
+$targetFile = $target_dir . $sessionId . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
@@ -28,6 +28,7 @@ if ($uploadOk == 0) {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
         echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
     } else {
+        echo "target file" . $targetFile
         echo "Sorry, there was an error uploading your file.";
     }
 }
