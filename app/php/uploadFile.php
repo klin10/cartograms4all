f65bb7c91be41a60da18ea0b8dfb010666beaee3
jlin53 <jlin53@ucsc.edu>
2017-07-14 03:29:19 +0000

    added debug

--- app/php/uploadFile.php
+++ app/php/uploadFile.php
@@ -3,11 +3,12 @@
 
 //$sessionId = $_COOKIE['c4a_session_id'];
 $sessionId = 10;
-$targetDir = "/data";
+$targetDir = "/data/";
 $targetFile = $target_dir . $sessionId . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 
+echo 'file check exist';
 // Check if file already exists
 if (file_exists($targetFile)) {
     echo "Sorry, file already exists.";
@@ -19,7 +20,7 @@ if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
 }
-
+echo 'ready to upload';
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded.";
