be37a0d2e1ea8a01ad89fca69973e4cde0a89380
jlin53 <jlin53@ucsc.edu>
2017-07-15 04:35:39 +0000

    DEPLOY

--- app/uploader/exporter.php
+++ app/uploader/exporter.php
@@ -4,7 +4,7 @@ function writeSettingsFile(){
     if(!empty($_POST['data'])){
         $settings_string = $_POST['data'];
         $session_file_name = "upload/";
-        $session_id = $_POST['name'];
+        $session_id = "123";
         $session_file_name .= ($session_id .= ".json");
         file_put_contents($session_file_name, $settings_string);
     }
