cfca182b3f408f33e9b564ec984387c296872ec0
jlin53 <jlin53@ucsc.edu>
2017-07-15 04:17:05 +0000

    DEPLOY

--- app/uploader/upload-manager.php
+++ app/uploader/upload-manager.php
@@ -3,11 +3,11 @@ header('Access-Control-Allow-Origin: *');
 // Check if the form was submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Check if file was uploaded without errors
-    if(isset($_POST['input_csv']) && $_POST['input_csv']['error'] == 0){
+    if(isset($_POST["input_csv"]) && $_POST["input_csv"]['error'] == 0){
     
        // Verify file size - 5MB maximum
-        $maxsize = 5 * 1024 * 1024;
-        if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
+       //$maxsize = 5 * 1024 * 1024;
+       // if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
     
        // Check whether file exists before uploading it
         if(file_exists("upload/" . $_POST["input_csv"]["name"])){
