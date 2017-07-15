ab5324e391eb0ba8860422e5578afa77e286733a
jlin53 <jlin53@ucsc.edu>
2017-07-15 02:26:29 +0000

    DEPLOY

--- app/uploader/upload-manager.php
+++ app/uploader/upload-manager.php
@@ -5,11 +5,7 @@ header('Access-Control-Allow-Origin: *');
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     // Check if file was uploaded without errors
-    if(isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0){
-        $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
-        $filename = $_FILES["photo"]["name"];
-        $filetype = $_FILES["photo"]["type"];
-        $filesize = $_FILES["photo"]["size"];
+    if(isset($_FILES["input_csv"]) && $_FILES["input_csv"]["error"] == 0){
     
        // Verify file size - 5MB maximum
         $maxsize = 5 * 1024 * 1024;
@@ -18,10 +14,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         // Verify MYME type of the file
         if(in_array($filetype, $allowed)){
             // Check whether file exists before uploading it
-            if(file_exists("upload/" . $_FILES["photo"]["name"])){
-                echo $_FILES["photo"]["name"] . " is already exists.";
+            if(file_exists("upload/" . $_FILES["input_csv"]["name"])){
+                echo $_FILES["input_csv"]["name"] . " is already exists.";
             } else{
-                if (move_uploaded_file($_FILES["photo"]["tmp_name"], "upload/" . $_FILES["photo"]["name"])){
+                if (move_uploaded_file($_FILES["input_csv"]["tmp_name"], "upload/" . $_FILES["input_csv"]["name"])){
                   echo "Your file was uploaded successfully.";
                 }
                 else {
@@ -33,7 +29,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             echo "Error: There was a problem uploading your file. Please try again."; 
         }
     } else{
-        echo "Error: " . $_FILES["photo"]["error"];
+        echo "Error: " . $_FILES["input_csv"]["error"];
     }
 }
 ?>
