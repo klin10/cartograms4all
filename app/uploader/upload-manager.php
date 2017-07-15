a8a365d2bc4a96079c835c9d6f51da04ef381168
jlin53 <jlin53@ucsc.edu>
2017-07-15 03:01:31 +0000

    DEPLOY

--- app/uploader/upload-manager.php
+++ app/uploader/upload-manager.php
@@ -11,24 +11,18 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         $maxsize = 5 * 1024 * 1024;
         if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
     
-        // Verify MYME type of the file
-        if(in_array($filetype, $allowed)){
-            // Check whether file exists before uploading it
-            if(file_exists("upload/" . $_FILES["input_csv"]["name"])){
-                echo $_FILES["input_csv"]["name"] . " is already exists.";
-            } else{
-                if (move_uploaded_file($_FILES["input_csv"]["tmp_name"], "upload/" . $_FILES["input_csv"]["name"])){
-                  echo "Your file was uploaded successfully.";
-                }
-                else {
+       // Check whether file exists before uploading it
+        if(file_exists("upload/" . $_FILES["input_csv"]["name"])){
+          echo $_FILES["input_csv"]["name"] . " is already exists.";
+        } else{
+            if (move_uploaded_file($_FILES["input_csv"]["tmp_name"], "upload/" . $_FILES["input_csv"]["name"])){
+              echo "Your file was uploaded successfully.";
+            } else {
                   echo "upload fail";
                   print_r($_FILES);
-		}
-            } 
-        } else{
-            echo "Error: There was a problem uploading your file. Please try again."; 
-        }
-    } else{
+	    }
+        }    
+     } else{
         echo "Error: " . $_FILES["input_csv"]["error"];
     }
 }
